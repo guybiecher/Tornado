824a887e0f83dc117b9366b29f350131ef0559af
LiadBerko <liad.berko@hpe.com>
2016-08-23 14:22:32 +0300

    updates

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
-/**
- * Created by berkol on 20/08/2016.
- */
+
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session');
+var cookieParser = require('cookie-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+
+
+var server = app.listen(3000,function(){
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("App listening at http://%s:%s", host, port);
+
+});
