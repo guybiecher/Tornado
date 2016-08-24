fbe673e10894e2f81739d9efeff1e32f9e9ad0bb
LiadBerko <liad.berko@hpe.com>
2016-08-24 15:13:59 +0300

    updates

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-/**
- * Created by berkol on 20/08/2016.
- */
+function getChats(str) {
+    var xhttp;
+    if (str == "") {
+        document.getElementById("chats_list").innerHTML = "";
+        return;
+    }
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("chats_list").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "getchats.asp?q="+str, true);
+    xhttp.send();
+}
--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 
+app.get('/chats', function(req,res){
+    // TODO:send query to db for first 10 chats, insert by loob into <li> element and send back
+    //client side function should trigger another request after injecting first 10 successfully
+});
+
+app.get('/contacts', function(req,res){
+   // TODO:send query to db for first 10 contacts, insert by loob into <li> element and send back
+    //client side function should trigger another request after injecting first 10 successfully
+});
 
 
 var server = app.listen(3000,function(){
