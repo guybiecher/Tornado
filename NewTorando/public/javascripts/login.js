b15ab64639f1b0df748e9c76e55772981cb78468
tomerbarshishat <tomer.barshishat@gmail.com>
2016-08-24 20:09:53 +0300

    add files

--- NewTorando/public/javascripts/login.js
+++ NewTorando/public/javascripts/login.js
@@ -1,3 +1,30 @@
 /**
  * Created by tomerbarshishat on 24/08/2016.
  */
+var username = $("#userName").valueOf();
+var password = $("#password").valueOf();
+
+
+$("#login_button").click(function() {
+    var user = {
+        name: $("userName").val(),
+        password:$("#password").val()
+    }
+
+    $.ajax({
+        url: '/login',
+        type: 'post',
+        dataType: 'json',
+        succsess: function(data) {
+            if (data == "ERROR_1") {
+                $("#command").html("The username\\password is incorrect");
+            }
+        },
+
+        data: user
+
+
+    });
+
+
+});
--- NewTorando/public/javascripts/register.js
+++ NewTorando/public/javascripts/register.js
@@ -1,3 +1,37 @@
 /**
  * Created by tomerbarshishat on 24/08/2016.
  */
+
+var username = $("#userName").valueOf();
+var password = $("#password").valueOf();
+var Retype_password = $("#Retype_password").valueOf();
+
+if(password =! Retype_password){
+    $("#error").html("the password is not match");
+}
+$("#Register").click(function() {
+    var person = {
+        name: $("userName").val(),
+        password:$("#password").val()
+    }
+
+    $.ajax({
+        url: '/register',
+        type: 'post',
+        dataType: 'json',
+        succsess: function(data) {
+            if (data == "ERROR_1") {
+                $("#command").html("The username is invalid");
+            } else if (data == "ERROR_2") {
+                $("#command").html("The password is invalid");
+            }
+        },
+
+        data: person
+
+
+    });
+
+
+});
+
